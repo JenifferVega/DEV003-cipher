e303c75860d1c54d5a9fda703427b01d9cc13944
JenifferVega <kotomivega@gmail.com>
2022-11-30 10:17:12 -0500

    update js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,32 @@
-const cipher = {
-  // ...
+/*const generatePass = (base, lenght) => {
+  let password = "";
+  for (let x = 0; x < lenght; x++) {
+    let random = Math.floor(Math.random() * base.lenght);
+    password += base.charAt(random);
+  }
+  return password;
+};
+*/
+
+function cipher () {
+  let lenght = parseInt(inputLenght.value);
+  let alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz";
+
+  const symbols = "!#$%&/()=?¡*¨[]_:;+-_~^^><*";
+  const numbers = "123456789";
+  if(checkbox2.checked) base+=numbers;
+  if(checkbox3.checked) base+=symbols;
+
+  password.innerText = "AAA" ;/*generatePass(base, lenght);*/
+
 };
 
-export default cipher;
+  window.addEventListener("DOMContentLoaded", () =>{
+    GeneratePassword.addEventListener("click", () => {
+        cipher();
+    });
+
+  });
+
+
+//export default cipher;
